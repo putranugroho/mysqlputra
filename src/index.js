7bd2c5e55d31302b1f6333b19ff60f88460c638b
putranugroho <putraa.nugroho@gmail.com>
2019-07-31 10:43:07 +0700

    Front Page

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req,res) =>{
+    res.send('<h1>Selamat datang di Heroku API</h1>')
+})
+
 app.listen(port, () => {
     console.log('Berhasil Running di ' + port);
     
